530c750b1a2043b8c89942a8925ff2e648db2d4b
Gus Dahik <gdahik@email.com>
2021-06-07 11:02:23 -0400

    added comments

--- cypress/integration/examples/sample_spec.js
+++ cypress/integration/examples/sample_spec.js
@@ -2,6 +2,7 @@ describe('My First Test', () => {
     it('Visits site', () => {
         cy.visit('https://dacipad.whs.mil')
 
+        // check that the page contains 'Contact' link and click on it
         cy.contains('Contact').click()
 
         // should be a new URL which includes '/contact'
